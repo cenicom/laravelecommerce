95cad3fe501bfdf0eefb34c687e71885894928f5
Luis Eduardo González Vargas <luedgova1967@hotmail.com>
2021-10-22 08:18:57 -0500

    update quantity cart

--- app/Http/Livewire/CartComponent.php
+++ app/Http/Livewire/CartComponent.php
@@ -3,9 +3,43 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use Gloudemans\Shoppingcart\Facades\Cart;
 
 class CartComponent extends Component
 {
+    public function increaseQuantity($rowId)
+    {
+        $product = Cart::get($rowId);
+
+        $qty = $product->qty + 1;
+
+        Cart::update($rowId,$qty);
+    }
+
+    public function decreaseQuantity($rowId)
+    {
+        # code..
+        $product = Cart::get($rowId);
+
+        $qty = $product->qty - 1;
+
+        Cart::update($rowId,$qty);
+    }
+
+    public function destroy($rowId)
+    {
+        # code...
+        Cart::remove($rowId);
+
+        session()->flash('success_message','Item Eliminado Correctamente del Carrito de Compras');
+    }
+
+    public function destroyAll()
+    {
+        # code...
+        Cart::destroy();
+    }
+
     public function render()
     {
         return view('livewire.cart-component')
--- resources/views/livewire/cart-component.blade.php
+++ resources/views/livewire/cart-component.blade.php
@@ -33,16 +33,18 @@
                                 <div class="quantity">
                                     <div class="quantity-input">
                                         <input type="text" name="product-quatity" value="{{ $item->qty }}" data-max="120" pattern="[0-9]*" >
-                                        <a class="btn btn-increase" href="#"></a>
-                                        <a class="btn btn-reduce" href="#"></a>
+                                        <a class="btn btn-increase" href="#"
+                                            wire:click.prevent="increaseQuantity('{{ $item->rowId }}')"></a>
+                                        <a class="btn btn-reduce" href="#"
+                                            wire:click.prevent="decreaseQuantity('{{ $item->rowId }}')"></a>
                                     </div>
                                 </div>
                                 <div class="price-field sub-total">
                                     <p class="price">${{ $item->subtotal }}</p></div>
                                 <div class="delete">
-                                    <a href="#" class="btn btn-delete" title="">
+                                    <a href="#" wire:click.prevent="destroy('{{ $item->rowId }}')" class="btn btn-danger" title="">
                                         <span>Eliminar Elementos</span>
-                                        <i class="fa fa-times-circle" aria-hidden="true"></i>
+                                        <i class="fa fa-trash" aria-hidden="true"></i>
                                     </a>
                                 </div>
                             </li>
@@ -56,33 +58,46 @@
             <div class="summary">
                 <div class="order-summary">
                     <h4 class="title-box">Order Summary</h4>
-                    <p class="summary-info"><span class="title">Subtotal</span><b class="index">${{ Cart::subtotal() }}</b></p>
-                    <p class="summary-info"><span class="title">Impuesto</span><b class="index">${{ Cart::tax() }}</b></p>
-                    <p class="summary-info"><span class="title">Envió Gratis</span><b class="index">$0.00</b></p>
-                    <p class="summary-info total-info "><span class="title">Total</span><b class="index">${{ Cart::total() }}</b></p>
+                    <p class="summary-info"><span class="title">Subtotal</span>
+                        <b class="index">${{ Cart::subtotal() }}</b></p>
+                    <p class="summary-info"><span class="title">Impuesto</span>
+                        <b class="index">${{ Cart::tax() }}</b></p>
+                    <p class="summary-info"><span class="title">Envió Gratis</span>
+                        <b class="index">$0.00</b></p>
+                    <p class="summary-info total-info ">
+                        <span class="title">Total</span>
+                        <b class="index">${{ Cart::total() }}</b>
+                    </p>
                 </div>
                 <div class="checkout-info">
                     <label class="checkbox-field">
-                        <input class="frm-input " name="have-code" id="have-code" value="" type="checkbox"><span>I have promo code</span>
+                        <input class="frm-input " name="have-code" id="have-code" value="" type="checkbox">
+                        <span>I have promo code</span>
                     </label>
                     <a class="btn btn-checkout" href="checkout.html">Check out</a>
-                    <a class="link-to-shop" href="shop.html">Continue Shopping<i class="fa fa-arrow-circle-right" aria-hidden="true"></i></a>
+                    <a class="link-to-shop" href="shop.html">Continue Shopping
+                        <i class="fa fa-arrow-circle-right" aria-hidden="true"></i></a>
                 </div>
                 <div class="update-clear">
-                    <a class="btn btn-clear" href="#">Clear Shopping Cart</a>
-                    <a class="btn btn-update" href="#">Update Shopping Cart</a>
+                    <a class="btn btn-clear" href="#" wire:click.prevent="destroyAll()">Eliminar Todos los Items</a>
+                    <a class="btn btn-update" href="#">Actualizar Todo</a>
                 </div>
             </div>
 
             <div class="wrap-show-advance-info-box style-1 box-in-site">
                 <h3 class="title-box">Most Viewed Products</h3>
                 <div class="wrap-products">
-                    <div class="products slide-carousel owl-carousel style-nav-1 equal-container" data-items="5" data-loop="false" data-nav="true" data-dots="false" data-responsive='{"0":{"items":"1"},"480":{"items":"2"},"768":{"items":"3"},"992":{"items":"3"},"1200":{"items":"5"}}' >
+                    <div class="products slide-carousel owl-carousel style-nav-1 equal-container"
+                    data-items="5" data-loop="false" data-nav="true" data-dots="false"
+                    data-responsive='{"0":{"items":"1"},"480":{"items":"2"},"768":{"items":"3"},"992":{"items":"3"},"1200":{"items":"5"}}' >
 
                         <div class="product product-style-2 equal-elem ">
                             <div class="product-thumnail">
                                 <a href="#" title="T-Shirt Raw Hem Organic Boro Constrast Denim">
-                                    <figure><img src="{{ asset('assets/images/products/digital_4.jpg') }}" width="214" height="214" alt="T-Shirt Raw Hem Organic Boro Constrast Denim"></figure>
+                                    <figure>
+                                        <img src="{{ asset('assets/images/products/digital_4.jpg') }}"
+                                        width="214" height="214" alt="T-Shirt Raw Hem Organic Boro Constrast Denim">
+                                    </figure>
                                 </a>
                                 <div class="group-flash">
                                     <span class="flash-item new-label">new</span>
